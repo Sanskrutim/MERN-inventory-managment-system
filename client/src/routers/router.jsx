c6af15d01033cb62429a29ff5cd256f73488a9f2
Sanskrutim <97704461+Sanskrutim@users.noreply.github.com>
2023-11-25 01:15:58 +0530

    Update router.jsx

--- client/src/routers/router.jsx
+++ client/src/routers/router.jsx
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
             {
                 path:"/book/:id",
                 element:<SingleBook/>,
-                loader:({params})=> fetch(`https://mern-inventory-managment-system.vercel.app/book/${params.id}`)
+                loader:({params})=> fetch(`https://mern-inventory-managment-system-nytbaszqa.vercel.app/book/${params.id}`)
             }
 
         ]
@@ -66,7 +66,7 @@ const router = createBrowserRouter([
                 
                 path:"/admin/dashboard/edit-books/:id",
                 element:<EditBooks/>,
-                loader: ({params}) => fetch(`https://mern-inventory-managment-system.vercel.app/book/${params.id}`)
+                loader: ({params}) => fetch(`https://mern-inventory-managment-system-nytbaszqa.vercel.app/book/${params.id}`)
             },
             
             
