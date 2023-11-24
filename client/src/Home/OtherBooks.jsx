c32c647c5a4a380b8f4daca0fef5528c18bda867
Sanskrutim <97704461+Sanskrutim@users.noreply.github.com>
2023-11-25 01:41:18 +0530

    Update OtherBooks.jsx

--- client/src/Home/OtherBooks.jsx
+++ client/src/Home/OtherBooks.jsx
@@ -7,7 +7,7 @@ const OtherBooks = () => {
         const [books, setBooks] = useState([]);
     
         useEffect( () => {
-            fetch("https://mern-inventory-managment-system-nytbaszqa.vercel.app/all-books").then(res => res.json()).then(data => setBooks(data.slice(0,8)))
+            fetch("http://localhost:8080/all-books").then(res => res.json()).then(data => setBooks(data.slice(0,8)))
         }, [])
       return (
         <BookCards books={books} headline="Other books"/>
