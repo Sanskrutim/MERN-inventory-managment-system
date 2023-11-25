6855e4d41f0e93760ae4f0171891759c7ca3fe6c
Sanskrutim <97704461+Sanskrutim@users.noreply.github.com>
2023-11-25 12:59:16 +0530

    Update BestSellBooks.jsx

--- client/src/Home/BestSellBooks.jsx
+++ client/src/Home/BestSellBooks.jsx
@@ -5,7 +5,7 @@ function BestSellerBooks() {
     const [books, setBooks] = useState([]);
 
     useEffect( () => {
-        fetch("https://mern-inventory-managment-system-pvgp.vercel.app/all-books").then(res => res.json()).then(data => setBooks(data.slice(0,8)))
+        fetch("https://mern-inventory-managment-system-server.vercel.app/all-books").then(res => res.json()).then(data => setBooks(data.slice(0,8)))
     }, [])
   return (
     <BookCards books={books} headline="Best seller books"/>
